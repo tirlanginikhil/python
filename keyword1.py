f3f350f7447a87b62d09c429e43c92995b83de9a
tirlanginikhil <nikhiltirlangi@gmail.com>
2026-07-27 12:39:44 +0530

    Update keyword1.py

--- keyword1.py
+++ keyword1.py
@@ -10,4 +10,40 @@ print("Total number of keywords:", len(keywords))
 print("\nPython Keywords:")
 for word in keywords:
     print(word)
+     #Python Keywords:
+False
+None
+True
+and
+as
+assert
+async
+await
+break
+class
+continue
+def
+del
+elif
+else
+except
+finally
+for
+from
+global
+if
+import
+in
+is
+lambda
+nonlocal
+not
+or
+pass
+raise
+return
+try
+while
+with
+yield
    
