28abb661ecbbea86ae1623c8e29ba6356e12eafd
tirlanginikhil <nikhiltirlangi@gmail.com>
2026-07-28 00:21:01 +0530

    Added Python program

---
+++ identifiers.py
@@ -0,0 +1,24 @@
+number = 25
+
+# Constant-style name (by convention, uppercase)
+MAX_VALUE = 100
+
+# Function name
+def display_message():
+    print("Function Name: display_message")
+
+# Class name
+class Student:
+    pass
+
+# Name using an underscore
+total_marks = 95
+
+# Print all identifiers
+print("Variable:", number)
+print("Constant-style Name: MAX_VALUE =", MAX_VALUE)
+print("Name with Underscore:", total_marks)
+
+display_message()
+
+print("Class Name: Student")
