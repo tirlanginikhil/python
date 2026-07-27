f034806fbc92f8bb06d46e88ef96f438ea5c74a1
tirlanginikhil <nikhiltirlangi@gmail.com>
2026-07-27 12:52:59 +0530

    Update keyword2.py

--- keyword2.py
+++ keyword2.py
@@ -8,3 +8,6 @@ if keyword.iskeyword(word):
     print(word, "is a Python keyword.")
 else:
     print(word, "is not a Python keyword.")
+    #output
+    Enter a word: else
+else is a Python keyword.
