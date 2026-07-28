c552292d44fa4b2e6dbd2e6e10289f9fc126020d
tirlanginikhil <nikhiltirlangi@gmail.com>
2026-07-28 07:36:04 +0530

    Added all folders and files

--- keywords.py/keyword3.py
+++ keywords.py/keyword3.py
@@ -3,12 +3,4 @@
 
 for = 5
 True = 10
- # File "c:\Users\Lenovo\.vscode\extensions\ms-python.debugpy-2026.6.0-win32-x64\bundled\libs\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_runpy.py", line 309, in run_path
-    code, fname = _get_code_from_file(run_name, path_name)
-                  ~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^
-  File "c:\Users\Lenovo\.vscode\extensions\ms-python.debugpy-2026.6.0-win32-x64\bundled\libs\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_runpy.py", line 283, in _get_code_from_file
-    code = compile(f.read(), fname, "exec")
-  File "C:\Users\Lenovo\Desktop\nikhil\keywords.py\keyword3.py", line 4
-    for = 5
-        ^
-SyntaxError: invalid syntax
+ 
