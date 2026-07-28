2d6c5294df0ba03884a2f27427a10aea2eef2bfc
tirlanginikhil <nikhiltirlangi@gmail.com>
2026-07-28 07:24:07 +0530

    Your commit message

---
+++ keywords.py/variables.py/variables1.py
@@ -0,0 +1,18 @@
+# Declaring variables
+name = "Nikhil"
+age = 18
+height = 156.0
+is_student = True
+
+# Printing variables and their data types
+print("Name:", name)
+print("Type of name:", type(name))
+
+print("Age:", age)
+print("Type of age:", type(age))
+
+print("Height:", height)
+print("Type of height:", type(height))
+
+print("Student:", is_student)
+print("Type of student:", type(is_student))
